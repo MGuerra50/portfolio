9e81ce4698bb6ff065c7040393d713d4bb896a7b
Matheus <matheusguerra50@gmail.com>
2023-12-22 16:58:59 -0300

    Finalização do funcionamento do componente de slide

--- src/componentes/Slides/index.js
+++ src/componentes/Slides/index.js
@@ -1,33 +1,50 @@
+import { useEffect, useState } from 'react';
 import styles from './Slides.module.css';
 
-export default function Slides ({ imagens, tempo = 2, posicao }) {    
+function verificaEstilo (index, slideAtual){
+                        
+    if(index === 0){
+        return {marginRight: - slideAtual + '%'}
+    } else {
+        return {marginRight: 0}
+    }
+}
 
-    return (
+export default function Slides ({ imagens, tempo = 5 }) {  
+   
+    const [slideAtual, setSlideAtual] = useState(0);
+    const movimentoMaxSlide = (imagens.length * 100) - 100;
+
+    useEffect(()=>{
+        const intervaloSlides = setInterval(()=>{
+            
+            if(slideAtual === movimentoMaxSlide){
+                setSlideAtual(()=>0);
+            } else {
+                setSlideAtual(()=>slideAtual + 100);
+            }
+
+        }, tempo*1000);
+
+        return() => clearInterval(intervaloSlides);
+    }, [movimentoMaxSlide, slideAtual, tempo]);
+    
+    if (!imagens || imagens.length === 0) {
+        return <div>Nenhuma imagem disponível.</div>;
+      }      
+
+        return (
         <div className={styles.slideShow}>
-            <div className={styles.slides}>
-                <input type='radio' name='botaoCircular' className={styles.circulo1}/>
-                <input type='radio' name='botaoCircular' className={styles.circulo2}/>
-                <input type='radio' name='botaoCircular' className={styles.circulo3}/>
-                <input type='radio' name='botaoCircular' className={styles.circulo4}/>
-            </div>
+
             {imagens.map((imagemAtual, index)=>{
+                    
                 return (
-                    <div className={styles.elementoSlide}>
-                        <img key={index} src={imagemAtual} alt={`slide ${index+1}`}/>
+                    <div key={index} style={verificaEstilo(index, slideAtual)} className={styles.elementoSlide}>
+                        <img src={imagemAtual} alt={`slide ${index+1}`}/>
                     </div>);
-            })}
-            <div className={styles.navegacaoAutomatica}>
-                <div className={styles.botao1Auto}/>
-                <div className={styles.botao2Auto}/>
-                <div className={styles.botao3Auto}/>
-                <div className={styles.botao4Auto}/>
-            </div>
-            <div className={styles.navegacaoManual}>
-                <label for = {styles.circulo1} className={styles.botaoManual}/>
-                <label for = {styles.circulo2} className={styles.botaoManua2}/>
-                <label for = {styles.circulo3} className={styles.botaoManua3}/>
-                <label for = {styles.circulo4} className={styles.botaoManua4}/>
-            </div>
+                }
+                )}
+
         </div>
     );
 }
