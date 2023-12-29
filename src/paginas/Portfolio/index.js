532f81ca7dd54a7b5b679e157176317fd983e3cd
Matheus <matheusguerra50@gmail.com>
2023-12-29 16:36:00 -0300

    Inicio do carrossel de projetos

--- src/paginas/Portfolio/index.js
+++ src/paginas/Portfolio/index.js
@@ -2,10 +2,16 @@ import InfoProjeto from '../../componentes/InfoProjeto';
 import styles from './Portfolio.module.css';
 
 export default function Portfolio () {
+
+    const listaProjetos = ['Jogo Da Velha', 'Chat', 'Site de Portfólio', 'E commerce', 'Xadrez', 'Página de Login', 'Criador de Mapas Mentais', 'SnowBall', 'IA', 'Saas', 'Criador de Temas WordPress', 'Página de Captura'];
+
     return (
         <div>
-            <div>
-                <h1 className={styles.titulo}>Oi</h1>
+            <div className={styles.carrosselEspaco}>
+                <h1 className={styles.titulo}>Meus projetos</h1>
+                <div className={styles.carrossel}>
+                    
+                </div>
             </div>
             <InfoProjeto />
         </div>
