91aa92b579fc6a7d1f1e3ee0cf656b4730c429c0
Matheus <matheusguerra50@gmail.com>
2023-12-31 17:23:07 -0300

    Desenvolvendo o carrossel da página de portfólio

--- src/paginas/Portfolio/index.js
+++ src/paginas/Portfolio/index.js
@@ -1,54 +1,92 @@
 import InfoProjeto from '../../componentes/InfoProjeto';
 import styles from './Portfolio.module.css';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import imagem1 from '../../componentes/ImagensGamesTeste/1.png';
+import imagem2 from '../../componentes/ImagensGamesTeste/2.png';
+import imagem3 from '../../componentes/ImagensGamesTeste/3.png';
+import imagem4 from '../../componentes/ImagensGamesTeste/4.png';
+import imagem5 from '../../componentes/ImagensGamesTeste/5.png';
+import imagem6 from '../../componentes/ImagensGamesTeste/6.png';
+import imagem7 from '../../componentes/ImagensGamesTeste/7.png';
+import imagem8 from '../../componentes/ImagensGamesTeste/8.png';
+import imagem9 from '../../componentes/ImagensGamesTeste/9.png';
+import imagem10 from '../../componentes/ImagensGamesTeste/10.png';
+import imagem11 from '../../componentes/ImagensGamesTeste/11.png';
+import imagem12 from '../../componentes/ImagensGamesTeste/12.png';
 
 export default function Portfolio () {
 
     const listaProjetos = [
         {
             nome: 'Jogo Da Velha',
-            imagem: 'Imagem'
+            imagem: imagem1
         }, {
             nome: 'Chat',
-            imagem: 'Imagem'
+            imagem: imagem2
         },{
             nome: 'Site de Portfólio',
-            imagem: 'Imagem'
+            imagem: imagem3
         },{
             nome: 'E commerce',
-            imagem: 'Imagem'
+            imagem: imagem4
         },{
             nome: 'Xadrez',
-            imagem: 'Imagem'
+            imagem: imagem5
         },{
             nome: 'Página de Login',
-            imagem: 'Imagem'
+            imagem: imagem6
         },{
             nome: 'Criador de Mapas Mentais',
-            imagem: 'Imagem'
+            imagem: imagem7
         },{
             nome: 'SnowBall',
-            imagem: 'Imagem'
+            imagem: imagem8
         },{
             nome: 'IA',
-            imagem: 'Imagem'
+            imagem: imagem9
         },{
             nome: 'Saas',
-            imagem: 'Imagem'
+            imagem: imagem10
         },{
             nome: 'Criador de Temas WordPress',
-            imagem: 'Imagem'
+            imagem: imagem11
         },{
             nome: 'Página de Captura',
-            imagem: 'Imagem'
+            imagem: imagem12
         }
     ];
+
+    function verificandoEstilo (index, posicaoProjeto){
+        /* zIndex: -1 em um momento em que o elemento já tenha obrigatóriamente saido da tela, pois some da vizão */
+        if(index === 0){
+            return {marginRight: - posicaoProjeto + '%', backgroundImage: `url(${posicaoProjeto.imagem})`}
+        } else {
+            return {marginRight: 0, backgroundImage: `url(${posicaoProjeto.imagem})`}
+        }
+    }
 
     return (
         <div>
             <div className={styles.carrosselEspaco}>
                 <h1 className={styles.titulo}>Meus projetos</h1>
                 <div className={styles.carrossel}>
-                    {listaProjetos.map((projeto)=><div>{projeto.nome}</div>)}
+                    <div className={styles.botao}>
+                        <IoIosArrowBack className={styles.iconeBotao}/>
+                    </div>
+                    <div className={styles.itensCarrossel}>
+                        {listaProjetos.map((projeto, index)=>{
+                            return(
+                                <div key={index} style={verificandoEstilo(index, projeto)} className={styles.espacoImagem}>
+                                    <div style={{width: '225px'}}>
+                                        <h2 className={styles.tituloProjeto}>{projeto.nome}</h2>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className={styles.botao}>
+                        <IoIosArrowForward className={styles.iconeBotao}/>
+                    </div>
                 </div>
             </div>
             <InfoProjeto />
