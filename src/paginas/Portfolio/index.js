27ddae60ea919ca315b25958fd2b986a2c0d1277
Matheus <matheusguerra50@gmail.com>
2023-12-30 23:49:00 -0300

    inicio de criação do carrossel

--- src/paginas/Portfolio/index.js
+++ src/paginas/Portfolio/index.js
@@ -3,14 +3,52 @@ import styles from './Portfolio.module.css';
 
 export default function Portfolio () {
 
-    const listaProjetos = ['Jogo Da Velha', 'Chat', 'Site de Portfólio', 'E commerce', 'Xadrez', 'Página de Login', 'Criador de Mapas Mentais', 'SnowBall', 'IA', 'Saas', 'Criador de Temas WordPress', 'Página de Captura'];
+    const listaProjetos = [
+        {
+            nome: 'Jogo Da Velha',
+            imagem: 'Imagem'
+        }, {
+            nome: 'Chat',
+            imagem: 'Imagem'
+        },{
+            nome: 'Site de Portfólio',
+            imagem: 'Imagem'
+        },{
+            nome: 'E commerce',
+            imagem: 'Imagem'
+        },{
+            nome: 'Xadrez',
+            imagem: 'Imagem'
+        },{
+            nome: 'Página de Login',
+            imagem: 'Imagem'
+        },{
+            nome: 'Criador de Mapas Mentais',
+            imagem: 'Imagem'
+        },{
+            nome: 'SnowBall',
+            imagem: 'Imagem'
+        },{
+            nome: 'IA',
+            imagem: 'Imagem'
+        },{
+            nome: 'Saas',
+            imagem: 'Imagem'
+        },{
+            nome: 'Criador de Temas WordPress',
+            imagem: 'Imagem'
+        },{
+            nome: 'Página de Captura',
+            imagem: 'Imagem'
+        }
+    ];
 
     return (
         <div>
             <div className={styles.carrosselEspaco}>
                 <h1 className={styles.titulo}>Meus projetos</h1>
                 <div className={styles.carrossel}>
-                    
+                    {listaProjetos.map((projeto)=><div>{projeto.nome}</div>)}
                 </div>
             </div>
             <InfoProjeto />
