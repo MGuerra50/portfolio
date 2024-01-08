41d325153141da5b068d20b9af765bf7c5e11815
Matheus <matheusguerra50@gmail.com>
2024-01-08 10:49:10 -0300

    Concerto do bug inesperado

--- src/paginas/Portfolio/index.js
+++ src/paginas/Portfolio/index.js
@@ -78,7 +78,7 @@ export default function Portfolio () {
         }
     ];
 
-    const [posicaoCarrossel, setPosicaoCarrossel] = useState('');
+    const [posicaoCarrossel, setPosicaoCarrossel] = useState(0);
 
     function verificandoEstilo(index, posicaoProjeto) {
         const marginRight = index === 0 ? -posicaoCarrossel : 0;
