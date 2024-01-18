9df3598dd53a63d1741dd65e2ab605fa82f3a6cb
Matheus <matheusguerra50@gmail.com>
2024-01-18 09:04:02 -0300

    Adição de Scroll para cima a cada link clicado

--- src/componentes/BotaoPadrao/index.js
+++ src/componentes/BotaoPadrao/index.js
@@ -1,7 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './BotaoPadrao.module.css';
+import { useEffect } from 'react';
 
 export default function BotaoPadrao({ texto, to }) {
+    const { pathname } = useLocation();
+
+        useEffect(() => {
+            window.scrollTo(0, 0);
+        }, [pathname]);
+
     return (
         <Link to={to}>
             <button className={styles.botao}>
