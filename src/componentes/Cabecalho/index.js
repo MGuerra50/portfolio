39fac876c09a4e2986a6c1b95aaef9b4c4f52188
Matheus <matheusguerra50@gmail.com>
2024-01-19 14:02:32 -0300

    Adicição de logo ao site

--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -1,16 +1,28 @@
+import styled from 'styled-components';
 import MenuPrincipal from '../MenuPrincipal';
 import styles from './Cabecalho.module.css';
+import logo from './Logo/logo2.png';
+import { Link } from 'react-router-dom';
 
-export default function Cabecalho () {
+const LogoSite = styled.img`
+    width: 100%;
+    height: auto;
+    opacity: 100%;
+`
+
+export default function Cabecalho() {
     return (
         <div className={styles.delimitacaoCabecalho}>
-            
-            <div className={styles.cabecalho}>
 
-                <div className={styles.logo}></div>
-                
+            <div className={styles.cabecalho}>
+                <div className={styles.logo}>
+                    <Link to={'/'}>
+                        <LogoSite src={logo} alt='Imagem de logo do site' />
+                    </Link>
+                </div>
+
                 <div className={styles.menuPrincipal}>
-                    <MenuPrincipal cor='#ffffff'/>
+                    <MenuPrincipal cor='#ffffff' />
                 </div>
 
             </div>
