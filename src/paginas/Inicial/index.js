c05da1cea5f9acc630303703ba6d517ad8e2bd73
Matheus <matheusguerra50@gmail.com>
2024-01-20 07:32:38 -0300

    Atualização de estilo da página inicial

--- src/paginas/Inicial/index.js
+++ src/paginas/Inicial/index.js
@@ -6,24 +6,40 @@ import imagem3 from '../../imagensTeste/3.png';
 import Resumo from '../../componentes/Resumo';
 import BotaoPadrao from '../../componentes/BotaoPadrao';
 import Certificacoes from '../../componentes/Certificacoes';
+import styled from 'styled-components';
 
-export default function Inicial () {
+const EstiloPagina = styled.div`
+    position: relative;
+    height: auto;
+    width: 1220px;
+    top: 110px;
+    right: -50%;
+    transform: translateX(-50%);
+    justify-content: center;
+    background-color: transparent;
+    border: 1px solid #033473;
+    box-shadow: 0px 0px 7px 0px #033473ac;
+    border-radius: 7px;
+    overflow: hidden;
+`
+
+export default function Inicial() {
 
     const testeSlide = [imagem1, imagem2, imagem3];
     const conhecimentos = ['React', 'NodeJs', 'PostgreSQL', 'MER', 'HTML', 'CSS'];
 
     return (
-        <>
+        <EstiloPagina>
             <div className={styles.slideShow}>
 
-                <Slides imagens={testeSlide}/>
+                <Slides imagens={testeSlide} />
 
                 <div className={styles.backgroundSlide}>
                     <span className={styles.movimentoSlide}>
-                    
+
                         <div className={styles.botaoParteExterna}>
                         </div>
-                    
+
                         <span className={styles.espacoSlide}>
                             <div className={styles.item3DoGrid}></div>
                             <div></div>
@@ -32,25 +48,25 @@ export default function Inicial () {
                                     Olá, me chamo  Matheus
                                 </h1>
                                 <h3 className={styles.textoSlide}>
-                                    Programador full-stack, e graduando em Engenharia de Computação 
+                                    Programador full-stack, e graduando em Engenharia de Computação
                                 </h3>
                             </div>
                             <div></div>
                             <div className={styles.item3DoGrid}></div>
                             <div className={styles.item6DoGrid}>
-                                <BotaoPadrao texto='Portfólio' to={'/portfolio'}/>
-                                <BotaoPadrao texto='Contato' to={'/contato'}/>
+                                <BotaoPadrao texto='Portfólio' to={'/portfolio'} />
+                                <BotaoPadrao texto='Contato' to={'/contato'} />
                             </div>
                         </span>
-                    
+
                         <div className={styles.botaoParteExterna}>
                         </div>
                     </span>
                 </div>
             </div>
-            <Resumo conhecimentos = {conhecimentos}/>
-            <Certificacoes/>
-        </>
-        
+            <Resumo conhecimentos={conhecimentos} />
+            <Certificacoes />
+        </EstiloPagina>
+
     );
 }
