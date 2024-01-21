5fe107f0b8af0fb13c480c6b60aead5c27bb4962
Matheus <matheusguerra50@gmail.com>
2024-01-21 13:36:27 -0300

    Finalização de atualização do novo design do site

--- src/paginas/Contato/index.js
+++ src/paginas/Contato/index.js
@@ -5,56 +5,59 @@ import { GrContact } from "react-icons/gr";
 import { FaInstagram } from "react-icons/fa6";
 import { CiLinkedin } from "react-icons/ci";
 import { HiOutlineMailOpen } from "react-icons/hi";
+import MoldeContainers from '../../componentes/MoldeContainers/Index';
 
 export default function Contato() {
     return (
-        <div className={styles.paginaContato}>
-            <h1 className={styles.tituloPagina}>Contato</h1>
-            <div className={styles.contatos}>
-                <div className={styles.campoContato}>
-                    <div className={styles.espacoIcon}>
-                        <IoPeopleSharp className={styles.icon} />
-                    </div>
-                    <h1 className={styles.tituloContato}>Solial</h1>
-                    <div className={styles.link}>
-                        <span className={styles.organizacao}>
-                            <a style={{color: 'rgb(16, 154, 255)', fontWeight: 700}} 
-                                href='https://www.instagram.com/mrguerra.matheus?igsh=MTJxMmF6c2s4dGZjZw=='
+        <MoldeContainers>
+            <div className={styles.paginaContato}>
+                <h1 className={styles.tituloPagina}>Contato</h1>
+                <div className={styles.contatos}>
+                    <div className={styles.campoContato}>
+                        <div className={styles.espacoIcon}>
+                            <IoPeopleSharp className={styles.icon} />
+                        </div>
+                        <h1 className={styles.tituloContato}>Solial</h1>
+                        <div className={styles.link}>
+                            <span className={styles.organizacao}>
+                                <a style={{ color: 'rgb(16, 154, 255)', fontWeight: 700 }}
+                                    href='https://www.instagram.com/mrguerra.matheus?igsh=MTJxMmF6c2s4dGZjZw=='
                                 ><FaInstagram /> Instagram
-                            </a>
-                        </span>
-                        <span className={styles.organizacao}>
-                            <a style={{color: 'rgb(16, 154, 255)', fontWeight: 700}} 
-                                href='https://www.linkedin.com/in/matheus-guerra-martins-0778b9158/'
+                                </a>
+                            </span>
+                            <span className={styles.organizacao}>
+                                <a style={{ color: 'rgb(16, 154, 255)', fontWeight: 700 }}
+                                    href='https://www.linkedin.com/in/matheus-guerra-martins-0778b9158/'
                                 ><CiLinkedin /> Linkedin
-                            </a>
-                        </span>
+                                </a>
+                            </span>
+                        </div>
                     </div>
-                </div>
-                <div className={styles.campoContato} style={{ borderLeft: '2px solid #033473', borderRight: '2px solid #033473' }}>
-                    <div className={styles.espacoIcon}>
-                        <MdOutlineMarkEmailRead className={styles.icon} />
-                    </div>
-                    <h1 className={styles.tituloContato}>Email</h1>
-                    <div className={styles.link}>
-                        <p style={{color: 'rgb(16, 154, 255)', fontWeight: 700}}
+                    <div className={styles.campoContato} style={{ borderLeft: '2px solid #033473', borderRight: '2px solid #033473' }}>
+                        <div className={styles.espacoIcon}>
+                            <MdOutlineMarkEmailRead className={styles.icon} />
+                        </div>
+                        <h1 className={styles.tituloContato}>Email</h1>
+                        <div className={styles.link}>
+                            <p style={{ color: 'rgb(16, 154, 255)', fontWeight: 700 }}
                             ><HiOutlineMailOpen /> matheusguerra5@hotmail.com
-                        </p>
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className={styles.campoContato}>
-                    <div className={styles.espacoIcon}>
-                        <GrContact className={styles.icon} />
-                    </div>
-                    <h1 className={styles.tituloContato}>Contato</h1>
-                    <div className={styles.link}>
-                        <a style={{color: 'rgb(16, 154, 255)', fontWeight: 700}} 
-                            href='https://wa.me/qr/6XAOSJLF5IUWL1'
+                    <div className={styles.campoContato}>
+                        <div className={styles.espacoIcon}>
+                            <GrContact className={styles.icon} />
+                        </div>
+                        <h1 className={styles.tituloContato}>Contato</h1>
+                        <div className={styles.link}>
+                            <a style={{ color: 'rgb(16, 154, 255)', fontWeight: 700 }}
+                                href='https://wa.me/qr/6XAOSJLF5IUWL1'
                             ><IoLogoWhatsapp /> WhatsApp
-                        </a>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </MoldeContainers>
     );
 }
