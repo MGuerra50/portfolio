064e933e2c3ef4ad26b06491978ca642a269a33d
Matheus <matheusguerra50@gmail.com>
2024-02-04 14:46:30 -0300

    Começando migração para json - Portfólio

--- src/paginas/Portfolio/index.js
+++ src/paginas/Portfolio/index.js
@@ -1,83 +1,84 @@
 import InfoProjeto from '../../componentes/InfoProjeto';
 import styles from './Portfolio.module.css';
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-import imagem1 from '../../componentes/ImagensGamesTeste/1.png';
-import imagem2 from '../../componentes/ImagensGamesTeste/2.png';
-import imagem3 from '../../componentes/ImagensGamesTeste/3.png';
-import imagem4 from '../../componentes/ImagensGamesTeste/4.png';
-import imagem5 from '../../componentes/ImagensGamesTeste/5.png';
-import imagem6 from '../../componentes/ImagensGamesTeste/6.png';
-import imagem7 from '../../componentes/ImagensGamesTeste/7.png';
-import imagem8 from '../../componentes/ImagensGamesTeste/8.png';
-import imagem9 from '../../componentes/ImagensGamesTeste/9.png';
-import imagem10 from '../../componentes/ImagensGamesTeste/10.png';
-import imagem11 from '../../componentes/ImagensGamesTeste/11.png';
-import imagem12 from '../../componentes/ImagensGamesTeste/12.png';
+import listaProjetos from './listaProjetos.json';
+// import imagem1 from '../../componentes/ImagensGamesTeste/1.png';
+// import imagem2 from '../../componentes/ImagensGamesTeste/2.png';
+// import imagem3 from '../../componentes/ImagensGamesTeste/3.png';
+// import imagem4 from '../../componentes/ImagensGamesTeste/4.png';
+// import imagem5 from '../../componentes/ImagensGamesTeste/5.png';
+// import imagem6 from '../../componentes/ImagensGamesTeste/6.png';
+// import imagem7 from '../../componentes/ImagensGamesTeste/7.png';
+// import imagem8 from '../../componentes/ImagensGamesTeste/8.png';
+// import imagem9 from '../../componentes/ImagensGamesTeste/9.png';
+// import imagem10 from '../../componentes/ImagensGamesTeste/10.png';
+// import imagem11 from '../../componentes/ImagensGamesTeste/11.png';
+// import imagem12 from '../../componentes/ImagensGamesTeste/12.png';
 import { useState } from 'react';
 import MoldeContainers from '../../componentes/MoldeContainers/Index';
 
 export default function Portfolio() {
 
-    const listaProjetos = [
-        {
-            nome: 'Jogo Da Velha',
-            imagem: imagem1
-        }, {
-            nome: 'Chat',
-            imagem: imagem2
-        }, {
-            nome: 'Portfólio',
-            imagem: imagem3
-        }, {
-            nome: 'E commerce',
-            imagem: imagem4
-        }, {
-            nome: 'Xadrez',
-            imagem: imagem5
-        }, {
-            nome: 'Página de Login',
-            imagem: imagem6
-        }, {
-            nome: 'Criador de Mapas Mentais',
-            imagem: imagem7
-        }, {
-            nome: 'SnowBall',
-            imagem: imagem8
-        }, {
-            nome: 'IA',
-            imagem: imagem9
-        }, {
-            nome: 'Saas',
-            imagem: imagem10
-        }, {
-            nome: 'Criador de Temas WordPress',
-            imagem: imagem11
-        }, {
-            nome: 'Página de Captura',
-            imagem: imagem12
-        }, {
-            nome: 'Página de Vendas Profissional',
-            imagem: imagem12
-        }, {
-            nome: 'SnowBall',
-            imagem: imagem8
-        }, {
-            nome: 'IA',
-            imagem: imagem9
-        }, {
-            nome: 'Saas',
-            imagem: imagem10
-        }, {
-            nome: 'Criador de Temas WordPress',
-            imagem: imagem11
-        }, {
-            nome: 'Página de Captura',
-            imagem: imagem12
-        }, {
-            nome: 'Página de Captura',
-            imagem: imagem12
-        }
-    ];
+    // const listaProjetos = [
+    //     {
+    //         nome: 'Jogo Da Velha',
+    //         imagem: imagem1
+    //     }, {
+    //         nome: 'Chat',
+    //         imagem: imagem2
+    //     }, {
+    //         nome: 'Portfólio',
+    //         imagem: imagem3
+    //     }, {
+    //         nome: 'E commerce',
+    //         imagem: imagem4
+    //     }, {
+    //         nome: 'Xadrez',
+    //         imagem: imagem5
+    //     }, {
+    //         nome: 'Página de Login',
+    //         imagem: imagem6
+    //     }, {
+    //         nome: 'Criador de Mapas Mentais',
+    //         imagem: imagem7
+    //     }, {
+    //         nome: 'SnowBall',
+    //         imagem: imagem8
+    //     }, {
+    //         nome: 'IA',
+    //         imagem: imagem9
+    //     }, {
+    //         nome: 'Saas',
+    //         imagem: imagem10
+    //     }, {
+    //         nome: 'Criador de Temas WordPress',
+    //         imagem: imagem11
+    //     }, {
+    //         nome: 'Página de Captura',
+    //         imagem: imagem12
+    //     }, {
+    //         nome: 'Página de Vendas Profissional',
+    //         imagem: imagem12
+    //     }, {
+    //         nome: 'SnowBall',
+    //         imagem: imagem8
+    //     }, {
+    //         nome: 'IA',
+    //         imagem: imagem9
+    //     }, {
+    //         nome: 'Saas',
+    //         imagem: imagem10
+    //     }, {
+    //         nome: 'Criador de Temas WordPress',
+    //         imagem: imagem11
+    //     }, {
+    //         nome: 'Página de Captura',
+    //         imagem: imagem12
+    //     }, {
+    //         nome: 'Página de Captura',
+    //         imagem: imagem12
+    //     }
+    // ];
 
     const [posicaoCarrossel, setPosicaoCarrossel] = useState(0);
 
@@ -123,9 +124,11 @@ export default function Portfolio() {
                         {listaProjetos.map((projeto, index) => {
                             return (
                                 <div key={index} style={verificandoEstilo(index, projeto)} className={styles.espacoImagem}>
+                                    
                                     <div onClick={() => trocaInfoProjeto(projeto.nome)} style={{ width: '225px' }}>
                                         <h2 className={styles.tituloProjeto}>{projeto.nome}</h2>
                                     </div>
+
                                 </div>
                             )
                         })}
