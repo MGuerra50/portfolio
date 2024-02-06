b8efe5c0c6ec9f8c8104cbb3dff17b57da98622c
Matheus <matheusguerra50@gmail.com>
2024-02-06 13:06:03 -0300

    Trabalhando na utilização do json

--- src/paginas/Portfolio/index.tsx
+++ src/paginas/Portfolio/index.tsx
@@ -1,21 +1,24 @@
 import InfoProjeto from '../../componentes/InfoProjeto/index.js';
 import styles from './Portfolio.module.css';
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-import listaProjetos from './listaProjetos.json';
+// import listaProjetos from './listaProjetos.json';
 import { useState } from 'react';
 import MoldeContainers from '../../componentes/MoldeContainers/Index.js';
-// import imagem1 from '../../componentes/ImagensGamesTeste/1.png';
-// import imagem2 from '../../componentes/ImagensGamesTeste/2.png';
-// import imagem3 from '../../componentes/ImagensGamesTeste/3.png';
-// import imagem4 from '../../componentes/ImagensGamesTeste/4.png';
-// import imagem5 from '../../componentes/ImagensGamesTeste/5.png';
-// import imagem6 from '../../componentes/ImagensGamesTeste/6.png';
-// import imagem7 from '../../componentes/ImagensGamesTeste/7.png';
-// import imagem8 from '../../componentes/ImagensGamesTeste/8.png';
-// import imagem9 from '../../componentes/ImagensGamesTeste/9.png';
-// import imagem10 from '../../componentes/ImagensGamesTeste/10.png';
-// import imagem11 from '../../componentes/ImagensGamesTeste/11.png';
-// import imagem12 from '../../componentes/ImagensGamesTeste/12.png';
+// import { url } from 'inspector';
+import imagem1 from '../../componentes/ImagensGamesTeste/1.png';
+import imagem2 from '../../componentes/ImagensGamesTeste/2.png';
+import imagem3 from '../../componentes/ImagensGamesTeste/3.png';
+import imagem4 from '../../componentes/ImagensGamesTeste/4.png';
+import imagem5 from '../../componentes/ImagensGamesTeste/5.png';
+import imagem6 from '../../componentes/ImagensGamesTeste/6.png';
+import imagem7 from '../../componentes/ImagensGamesTeste/7.png';
+import imagem8 from '../../componentes/ImagensGamesTeste/8.png';
+import imagem9 from '../../componentes/ImagensGamesTeste/9.png';
+import imagem10 from '../../componentes/ImagensGamesTeste/10.png';
+import imagem11 from '../../componentes/ImagensGamesTeste/11.png';
+import imagem12 from './ImagensGamesTeste/12.png';
+
+
 
 interface PropsListaProjetos {
     projeto: {
@@ -27,66 +30,66 @@ interface PropsListaProjetos {
 
 export default function Portfolio() {
 
-    // const listaProjetos = [
-    //     {
-    //         nome: 'Jogo Da Velha',
-    //         imagem: imagem1
-    //     }, {
-    //         nome: 'Chat',
-    //         imagem: imagem2
-    //     }, {
-    //         nome: 'Portfólio',
-    //         imagem: imagem3
-    //     }, {
-    //         nome: 'E commerce',
-    //         imagem: imagem4
-    //     }, {
-    //         nome: 'Xadrez',
-    //         imagem: imagem5
-    //     }, {
-    //         nome: 'Página de Login',
-    //         imagem: imagem6
-    //     }, {
-    //         nome: 'Criador de Mapas Mentais',
-    //         imagem: imagem7
-    //     }, {
-    //         nome: 'SnowBall',
-    //         imagem: imagem8
-    //     }, {
-    //         nome: 'IA',
-    //         imagem: imagem9
-    //     }, {
-    //         nome: 'Saas',
-    //         imagem: imagem10
-    //     }, {
-    //         nome: 'Criador de Temas WordPress',
-    //         imagem: imagem11
-    //     }, {
-    //         nome: 'Página de Captura',
-    //         imagem: imagem12
-    //     }, {
-    //         nome: 'Página de Vendas Profissional',
-    //         imagem: imagem12
-    //     }, {
-    //         nome: 'SnowBall',
-    //         imagem: imagem8
-    //     }, {
-    //         nome: 'IA',
-    //         imagem: imagem9
-    //     }, {
-    //         nome: 'Saas',
-    //         imagem: imagem10
-    //     }, {
-    //         nome: 'Criador de Temas WordPress',
-    //         imagem: imagem11
-    //     }, {
-    //         nome: 'Página de Captura',
-    //         imagem: imagem12
-    //     }, {
-    //         nome: 'Página de Captura',
-    //         imagem: imagem12
-    //     }
-    // ];
+    const listaProjetos = [
+        {
+            nome: 'Jogo Da Velha',
+            imagem: imagem1
+        }, {
+            nome: 'Chat',
+            imagem: imagem2
+        }, {
+            nome: 'Portfólio',
+            imagem: imagem3
+        }, {
+            nome: 'E commerce',
+            imagem: imagem4
+        }, {
+            nome: 'Xadrez',
+            imagem: imagem5
+        }, {
+            nome: 'Página de Login',
+            imagem: imagem6
+        }, {
+            nome: 'Criador de Mapas Mentais',
+            imagem: imagem7
+        }, {
+            nome: 'SnowBall',
+            imagem: imagem8
+        }, {
+            nome: 'IA',
+            imagem: imagem9
+        }, {
+            nome: 'Saas',
+            imagem: imagem10
+        }, {
+            nome: 'Criador de Temas WordPress',
+            imagem: imagem11
+        }, {
+            nome: 'Página de Captura',
+            imagem: imagem12
+        }, {
+            nome: 'Página de Vendas Profissional',
+            imagem: imagem12
+        }, {
+            nome: 'SnowBall',
+            imagem: imagem8
+        }, {
+            nome: 'IA',
+            imagem: imagem9
+        }, {
+            nome: 'Saas',
+            imagem: imagem10
+        }, {
+            nome: 'Criador de Temas WordPress',
+            imagem: imagem11
+        }, {
+            nome: 'Página de Captura',
+            imagem: imagem12
+        }, {
+            nome: 'Página de Captura',
+            imagem: imagem12
+        }
+    ];
 
     const [posicaoCarrossel, setPosicaoCarrossel] = useState(0);
 
@@ -107,11 +110,11 @@ export default function Portfolio() {
     }
     const [trocandoInfoProjeto, setTrocandoInfoProjeto] = useState('Jogo Da Velha');
 
-    function trocaInfoProjeto(nomeProjeto:string) {
+    function trocaInfoProjeto(nomeProjeto: string) {
         setTrocandoInfoProjeto(nomeProjeto);
     }
 
-    function verificandoEstilo({index, projeto}: PropsListaProjetos) {
+    function verificandoEstilo({ index, projeto }: PropsListaProjetos) {
         const marginRight = index === 0 ? -posicaoCarrossel : 0;
         const marginLeft = index === 0 ? 7 + 'px' : 0;
         const zIndex = index === 0 && posicaoCarrossel >= (225 * 5) ? -1 : 'auto';
@@ -129,9 +132,11 @@ export default function Portfolio() {
                         <IoIosArrowBack onClick={() => passagemCarrossel('-')} className={styles.iconeBotao} />
                     </div>
                     <div className={styles.itensCarrossel}>
+
                         {listaProjetos.map(({ projeto, index }: PropsListaProjetos) => {
+                            // {console.log('nome: '+projeto.nome)}
                             return (
-                                <div key={index} style={verificandoEstilo({index, projeto})} className={styles.espacoImagem}>
+                                <div key={index} style={{...verificandoEstilo({index, projeto}), backgroundImage: `url(${projeto.imagem})`}} className={styles.espacoImagem}>
 
                                     <div onClick={() => trocaInfoProjeto(projeto.nome)} style={{ width: '225px' }}>
                                         <h2 className={styles.tituloProjeto}>{projeto.nome}</h2>
