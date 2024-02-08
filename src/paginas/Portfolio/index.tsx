0f76b81e1156feff6e7ff93485a2dbf1bd904440
Matheus <matheusguerra50@gmail.com>
2024-02-08 09:49:46 -0300

    Encerramento da migração para json da página de portfolio

--- src/paginas/Portfolio/index.tsx
+++ src/paginas/Portfolio/index.tsx
@@ -1,100 +1,99 @@
 import InfoProjeto from '../../componentes/InfoProjeto/index.js';
 import styles from './Portfolio.module.css';
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-// import listaProjetos from './listaProjetos.json';
+
+import listaProjetosTeste from './listaProjetos.json';
+
 import { useState } from 'react';
 import MoldeContainers from '../../componentes/MoldeContainers/Index.js';
-import imagem1 from '../../componentes/ImagensGamesTeste/1.png';
-import imagem2 from '../../componentes/ImagensGamesTeste/2.png';
-import imagem3 from '../../componentes/ImagensGamesTeste/3.png';
-import imagem4 from '../../componentes/ImagensGamesTeste/4.png';
-import imagem5 from '../../componentes/ImagensGamesTeste/5.png';
-import imagem6 from '../../componentes/ImagensGamesTeste/6.png';
-import imagem7 from '../../componentes/ImagensGamesTeste/7.png';
-import imagem8 from '../../componentes/ImagensGamesTeste/8.png';
-import imagem9 from '../../componentes/ImagensGamesTeste/9.png';
-import imagem10 from '../../componentes/ImagensGamesTeste/10.png';
-import imagem11 from '../../componentes/ImagensGamesTeste/11.png';
-import imagem12 from './ImagensGamesTeste/12.png';
+// import imagem1 from '../../componentes/ImagensGamesTeste/1.png';
+// import imagem2 from '../../componentes/ImagensGamesTeste/2.png';
+// import imagem3 from '../../componentes/ImagensGamesTeste/3.png';
+// import imagem4 from '../../componentes/ImagensGamesTeste/4.png';
+// import imagem5 from '../../componentes/ImagensGamesTeste/5.png';
+// import imagem6 from '../../componentes/ImagensGamesTeste/6.png';
+// import imagem7 from '../../componentes/ImagensGamesTeste/7.png';
+// import imagem8 from '../../componentes/ImagensGamesTeste/8.png';
+// import imagem9 from '../../componentes/ImagensGamesTeste/9.png';
+// import imagem10 from '../../componentes/ImagensGamesTeste/10.png';
+// import imagem11 from '../../componentes/ImagensGamesTeste/11.png';
+// import imagem12 from '../../../public/projetos/12.png';
 
 
 
-interface PropsListaProjetos {
-    projeto: {
-        nome: string
-        imagem: any
-    }
-    index: number
+interface PropsProjetos {
+    nome: string
+    imagem: string
 }
 
 export default function Portfolio() {
 
-    const listaProjetos = [
-        {
-            nome: 'Jogo Da Velha',
-            imagem: imagem1
-        }, {
-            nome: 'Chat',
-            imagem: imagem2
-        }, {
-            nome: 'Portfólio',
-            imagem: imagem3
-        }, {
-            nome: 'E commerce',
-            imagem: imagem4
-        }, {
-            nome: 'Xadrez',
-            imagem: imagem5
-        }, {
-            nome: 'Página de Login',
-            imagem: imagem6
-        }, {
-            nome: 'Criador de Mapas Mentais',
-            imagem: imagem7
-        }, {
-            nome: 'SnowBall',
-            imagem: imagem8
-        }, {
-            nome: 'IA',
-            imagem: imagem9
-        }, {
-            nome: 'Saas',
-            imagem: imagem10
-        }, {
-            nome: 'Criador de Temas WordPress',
-            imagem: imagem11
-        }, {
-            nome: 'Página de Captura',
-            imagem: imagem12
-        }, {
-            nome: 'Página de Vendas Profissional',
-            imagem: imagem12
-        }, {
-            nome: 'SnowBall',
-            imagem: imagem8
-        }, {
-            nome: 'IA',
-            imagem: imagem9
-        }, {
-            nome: 'Saas',
-            imagem: imagem10
-        }, {
-            nome: 'Criador de Temas WordPress',
-            imagem: imagem11
-        }, {
-            nome: 'Página de Captura',
-            imagem: imagem12
-        }, {
-            nome: 'Página de Captura',
-            imagem: imagem12
-        }
-    ];
+    // const listaProjetos = [
+    //     {
+    //         nome: 'Jogo Da Velha',
+    //         imagem: imagem1
+    //     }, {
+    //         nome: 'Chat',
+    //         imagem: imagem2
+    //     }, {
+    //         nome: 'Portfólio',
+    //         imagem: imagem3
+    //     }, {
+    //         nome: 'E commerce',
+    //         imagem: imagem4
+    //     }, {
+    //         nome: 'Xadrez',
+    //         imagem: imagem5
+    //     }, {
+    //         nome: 'Página de Login',
+    //         imagem: imagem6
+    //     }, {
+    //         nome: 'Criador de Mapas Mentais',
+    //         imagem: imagem7
+    //     }, {
+    //         nome: 'SnowBall',
+    //         imagem: imagem8
+    //     }, {
+    //         nome: 'IA',
+    //         imagem: imagem9
+    //     }, {
+    //         nome: 'Saas',
+    //         imagem: imagem10
+    //     }, {
+    //         nome: 'Criador de Temas WordPress',
+    //         imagem: imagem11
+    //     }, {
+    //         nome: 'Página de Captura',
+    //         imagem: imagem12
+    //     }, {
+    //         nome: 'Página de Vendas Profissional',
+    //         imagem: imagem12
+    //     }, {
+    //         nome: 'SnowBall',
+    //         imagem: imagem8
+    //     }, {
+    //         nome: 'IA',
+    //         imagem: imagem9
+    //     }, {
+    //         nome: 'Saas',
+    //         imagem: imagem10
+    //     }, {
+    //         nome: 'Criador de Temas WordPress',
+    //         imagem: imagem11
+    //     }, {
+    //         nome: 'Página de Captura',
+    //         imagem: imagem12
+    //     }, {
+    //         nome: 'Página de Captura',
+    //         imagem: imagem12
+    //     }
+    // ];
 
     const [posicaoCarrossel, setPosicaoCarrossel] = useState(0);
 
     function passagemCarrossel(sentido: string) {
         const itemsPerPage = 5;
-        const totalItems = listaProjetos.length;
+        const totalItems = listaProjetosTeste.length;
         const totalPages = Math.ceil(totalItems / itemsPerPage);
 
         let novaPosicaoCarrossel = posicaoCarrossel;
@@ -113,7 +112,7 @@ export default function Portfolio() {
         setTrocandoInfoProjeto(nomeProjeto);
     }
 
-    function verificandoEstilo(projeto: {nome: string, imagem: any}, index:number /*PropsListaProjetos*/) {
+    function verificandoEstilo(projeto: { nome: string, imagem: string }, index: number) {
         const marginRight = index === 0 ? -posicaoCarrossel : 0;
         const marginLeft = index === 0 ? 7 + 'px' : 0;
         const zIndex = index === 0 && posicaoCarrossel >= (225 * 5) ? -1 : 'auto';
@@ -132,9 +131,24 @@ export default function Portfolio() {
                     </div>
                     <div className={styles.itensCarrossel}>
 
-                        {listaProjetos.map((projeto: {nome: string, imagem: any}, index:number) => {
+                        {/* {listaProjetos.map((projeto: { nome: string, imagem: any }, index: number) => {
                             return (
-                                <div key={index} style={{...verificandoEstilo(projeto, index), backgroundImage: `url(${projeto.imagem})`}} className={styles.espacoImagem}>
+                                <div key={index} style={{ ...verificandoEstilo(projeto, index), backgroundImage: `url(${projeto.imagem})` }} className={styles.espacoImagem}>
+
+                                    <div onClick={() => trocaInfoProjeto(projeto.nome)} style={{ width: '225px' }}>
+                                        <h2 className={styles.tituloProjeto}>{projeto.nome}</h2>
+                                    </div>
+
+                                </div>
+                            )
+                        })} */}
+                        {listaProjetosTeste.map((projeto: PropsProjetos, index: number) => {
+                            return (
+                                <div 
+                                    key={index} 
+                                    style={{ ...verificandoEstilo(projeto, index) }} 
+                                    className={styles.espacoImagem}
+                                >
 
                                     <div onClick={() => trocaInfoProjeto(projeto.nome)} style={{ width: '225px' }}>
                                         <h2 className={styles.tituloProjeto}>{projeto.nome}</h2>
