6f239932c93b3a3dcd17fbd580d1176917d23587
Matheus <matheusguerra50@gmail.com>
2024-02-11 08:56:47 -0300

    Alteração css - moldeContainers

--- src/componentes/MoldeContainers/Index.js
+++ src/componentes/MoldeContainers/Index.js
@@ -1,28 +1,9 @@
-import styled from 'styled-components';
-
-const EstiloPagina = styled.div`
-    position: relative;
-    height: auto;
-    width: 1220px;
-    top: 110px;
-    right: -50%;
-    transform: translateX(-50%);
-    justify-content: center;
-    background-color: transparent;
-    border: 1px solid #033473;
-    box-shadow: 0px 0px 10px 0px #033473ac;
-    border-radius: 7px;
-    overflow: hidden;
-
-    @media (max-width: 1219px) {
-    width: 95%;
-  }
-`
+import styles from './MoldeContainers.module.css';
 
 export default function MoldeContainers ({ children }) {
     return(
-        <EstiloPagina>
+        <div className={styles.estruturaPagina}>
             {children}
-        </EstiloPagina>
+        </div>
     );
 }
